6df28ffa429d9759611144607f7a9411e0b9e21b
kabradshaw1 <itllbok82@gmail.com>
2023-01-13 17:13:05 -0500

    started portfolio sectlion

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import { Container, Col, Card, CardColumns } from 'react-bootstrap';
+
+function Portfolio() {
+  return (
+    <Container>
+      <h2>Work</h2>
+      <CardColumns id="work">
+        <Card>
+
+        </Card>
+      </CardColumns>
+    </Container>  
+  )
+}
+
+export default Portfolio;
