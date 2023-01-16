0863b5b2e267073c8e0a6a56e2b5b15c4228efb2
kabradshaw1 <itllbok82@gmail.com>
2023-01-16 14:32:35 -0500

    added another app to projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -20,7 +20,17 @@ function Projects() {
               <Button variant='primary' href='https://github.com/kabradshaw1/National-Parks'>GitHub Repository</Button>        
             </Card.Body>
           </Card>
-      
+          <Card className='card'>
+            {/* <Card.Img variant='top' src={National} ></Card.Img> */}
+            <Card.Body>
+              <Card.Title>Fitness Tracker</Card.Title>
+              <Card.Text>
+                This project
+              </Card.Text>
+              <Button variant='primary' href='https://secure-gorge-89550.herokuapp.com//'>Deployed Site</Button>
+              <Button variant='primary' href='https://github.com/kabradshaw1/Fitness-Tracker'>GitHub Repository</Button>        
+            </Card.Body>
+          </Card>
       </Container>  
       <Container>
         <h2>Other Projects</h2>
