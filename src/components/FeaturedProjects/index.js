6758f84e85f6a2d1c9b474c7e70960f940da1a5f
kabradshaw1 <itllbok82@gmail.com>
2023-01-17 11:14:22 -0500

    making a mess with plans

---
+++ src/components/FeaturedProjects/index.js
@@ -0,0 +1,47 @@
+
+import React from "react";
+import { Container, Button, Card } from 'react-bootstrap';
+import National from '../../assets/img/screen_shots/National-Parks.PNG'
+
+function FeaturedProjects() {
+  featured = [
+    {
+      name: 'National Parks Search Engine',
+      description: 'This project serves to provide access to the APIs provided by nps.gov.  There is still a little work I would like to do for this project.',
+      gitub: 'https://github.com/kabradshaw1/National-Parks',
+      deployed: 'https://evening-reaches-42082.herokuapp.com/'
+    }
+  ]
+  return (
+    <>
+      <Container>
+        <h2>Featured Projects</h2>
+          {/* final group project */}
+          <Card className='card'>
+            <Card.Img variant='top' src={National} ></Card.Img>
+            <Card.Body>
+              <Card.Title>National Park Search</Card.Title>
+              <Card.Text>
+                This project serves to provide access to the APIs provided by nps.gov.  There is still a little work I would like to do for this project.
+              </Card.Text>
+              <Button variant='primary' href='https://evening-reaches-42082.herokuapp.com/'>Deployed Site</Button>
+              <Button variant='primary' href='https://github.com/kabradshaw1/National-Parks'>GitHub Repository</Button>        
+            </Card.Body>
+          </Card>
+          <Card className='card'>
+            {/* <Card.Img variant='top' src={National} ></Card.Img> */}
+            <Card.Body>
+              <Card.Title>Fitness Tracker</Card.Title>
+              <Card.Text>
+                This project
+              </Card.Text>
+              <Button variant='primary' href='https://secure-gorge-89550.herokuapp.com/'>Deployed Site</Button>
+              <Button variant='primary' href='https://github.com/kabradshaw1/Fitness-Tracker'>GitHub Repository</Button>        
+            </Card.Body>
+          </Card>
+      </Container>  
+    </>
+  )
+}
+
+export default FeaturedProjects;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,42 +1,16 @@
-
 import React from "react";
-import { Container, Button, Card } from 'react-bootstrap';
-import National from '../../assets/img/screen_shots/National-Parks.PNG'
+import FeaturedProjects from '../FeaturedProjects'
+import DeployedProjects from '../DeployedProjects'
+import OtherProjects from '../OtherProjects'
 
 function Projects() {
   return (
     <>
-      <Container>
-        <h2>Featured Projects</h2>
-          {/* final group project */}
-          <Card className='card'>
-            <Card.Img variant='top' src={National} ></Card.Img>
-            <Card.Body>
-              <Card.Title>National Park Search</Card.Title>
-              <Card.Text>
-                This project serves to provide access to the APIs provided by nps.gov.  There is still a little work I would like to do for this project.
-              </Card.Text>
-              <Button variant='primary' href='https://evening-reaches-42082.herokuapp.com/'>Deployed Site</Button>
-              <Button variant='primary' href='https://github.com/kabradshaw1/National-Parks'>GitHub Repository</Button>        
-            </Card.Body>
-          </Card>
-          <Card className='card'>
-            {/* <Card.Img variant='top' src={National} ></Card.Img> */}
-            <Card.Body>
-              <Card.Title>Fitness Tracker</Card.Title>
-              <Card.Text>
-                This project
-              </Card.Text>
-              <Button variant='primary' href='https://secure-gorge-89550.herokuapp.com/'>Deployed Site</Button>
-              <Button variant='primary' href='https://github.com/kabradshaw1/Fitness-Tracker'>GitHub Repository</Button>        
-            </Card.Body>
-          </Card>
-      </Container>  
-      <Container>
-        <h2>Other Projects</h2>
-      </Container>
+    <FeaturedProjects/>
+    <DeployedPorjects/>
+    <OtherProjects/>
     </>
   )
 }
 
-export default Projects;
+export default Projects
