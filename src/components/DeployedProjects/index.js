e684124aff3d975d4d0d3a62d7fc11e394687e1d
kabradshaw1 <itllbok82@gmail.com>
2023-01-18 09:28:16 -0500

    adding projects

--- src/components/DeployedProjects/index.js
+++ src/components/DeployedProjects/index.js
@@ -3,17 +3,24 @@ import React from "react";
 function DeployedPorjects() {
   const projects = [
     {
-      name: 'National Parks Search Engine',
-      description: 'This project serves to provide access to the APIs provided by nps.gov.  There is still a little work I would like to do for this project.',
-      gitub: 'https://github.com/kabradshaw1/National-Parks',
-      deployed: 'https://evening-reaches-42082.herokuapp.com/'
+      name: 'Deep Thoughts',
+      description: 'This is a social media site. This site uses MERN stack.',
+      gitub: 'https://github.com/kabradshaw1/deep-thoughts',
+      deployed: 'https://gentle-shore-15053.herokuapp.com/'
     },
     {
-      name: 'National Parks Search Engine',
-      description: 'This project serves to provide access to the APIs provided by nps.gov.  There is still a little work I would like to do for this project.',
-      gitub: 'https://github.com/kabradshaw1/National-Parks',
-      deployed: 'https://evening-reaches-42082.herokuapp.com/'
+      name: 'Photo Port',
+      description: 'This project is a portfolio for photographs.  This project has a React frontend only.',
+      gitub: 'https://github.com/kabradshaw1/photo-port',
+      deployed: 'https://kabradshaw1.github.io/photo-port/'
     },
+    {
+      name: 'Just Tech News',
+      description: 'This is a blog site.  This project uses an express server and MySQL database and demonstrates MVC and ORM.',
+      gitub: 'https://github.com/kabradshaw1/just-tech-news',
+      deployed: 'https://still-ocean-01779.herokuapp.com/'
+    },
+    
   ]
   return (
     <div></div>
