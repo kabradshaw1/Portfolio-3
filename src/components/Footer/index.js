804de4d210397a311ffb13045d771c3bdeb6f8f6
kabradshaw1 <itllbok82@gmail.com>
2023-01-21 20:59:02 -0500

    more learning bootstrap

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,42 @@
 import React from "react";
 import { VscGithubAlt } from "react-icons/vsc";
 import { AiOutlineLinkedin } from "react-icons/ai";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 function Footer() {
   return (
-    <footer className='bg-primary'>
-      <h2>Made by Kyle Bradshaw</h2>
-      <p>
-        <ul className='Row'>
-          <a className='Col' href="https://github.com/kabradshaw1">
-            <li className='text-dark'>
-              <VscGithubAlt />
-            </li>
-          </a>
-          <a className='Col' href="https://www.linkedin.com/in/kyle-bradshaw-15950988/">
-            <li className='text-dark'>
-              <AiOutlineLinkedin />
-            </li>
-          </a>
-        </ul>
-      </p>
+    <footer className='bg-primary container-fluid'>
+      <Container fluid>
+        <Row>
+          <Col>
+          <h2 >Made by Kyle Bradshaw</h2>
+          </Col>
+          <Col>
+            <p>
+              <ul >
+                <Row>
+                  <Col>
+                      <a  href="https://github.com/kabradshaw1">
+                        <li className='text-dark'>
+                          <VscGithubAlt />
+                        </li>
+                      </a>
+                  </Col>
+                  <Col>
+                      <a  href="https://www.linkedin.com/in/kyle-bradshaw-15950988/">
+                        <li className='text-dark'>
+                          <AiOutlineLinkedin />
+                        </li>
+                      </a>
+                  </Col>
+                </Row>
+              </ul>
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
